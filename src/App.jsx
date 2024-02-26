d89f34664321c7d7ce7ac26c2c41132257a39b80
Andrew Cao <78403245+idkuri@users.noreply.github.com>
2024-02-25 20:27:35 -0500

    css and 5 animes added

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,17 @@ import background from "./assets/background.png"
 import AnimeCard from './components/AnimeCard'
 import animelist from "./animelist.json"
 
+
 function App() {
 
   return (
     <div className="App">
-      <h1 className="app-title">My Top Animes This Season</h1>
+      <h1 className="app-title">My Top Animes</h1>
       <div className='anime-list'>
         {
           
           animelist.map((anime, key) => {
-            return <AnimeCard name={anime.name} key={key}/>
+            return <AnimeCard name={anime.name} mal_link={anime.mal_link} crunchy_link={anime.crunchy_link} img_path={anime.image_path} key={key}/>
           })
         }
       </div>
--- src/components/AnimeCard.jsx
+++ src/components/AnimeCard.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import cardImg from "../assets/sololeveling.jpg"
+import crunchy_logo from "../assets/crunchyroll.svg"
+import mal_logo from "../assets/myanimelist.svg"
 
-const AnimeCard = ({name}) => {
+const AnimeCard = ({name, mal_link, crunchy_link, img_path}) => {
     return (
         <div className='anime-card'>
             <h1 className='card-title'>{name}</h1>
             <div className='card-image-container'>
-                <img className='card-image' src={cardImg}></img>
+                <img className='card-image' src={img_path} alt="Solo Leveling"></img>
             </div>
             <div className='button-container'>
-                <button className='button'>MAL</button>
-                <button className='button'>Crunchyroll</button>
+                <button className='button-left' onClick={() => {window.open(mal_link)}}>
+                    <img className="button_logo" src={mal_logo}/>   
+                </button>
+                <button className='button-right' onClick={() => {window.open(crunchy_link)}}>
+                    <img className="button_logo" src={crunchy_logo} />
+                </button>
             </div>
         </div>
     );
